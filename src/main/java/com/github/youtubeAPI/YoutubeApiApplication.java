52d89e328b809cdef99e963b3a30faa88cb03434
Evgen-Mutagen <jeka190@mail.ru>
2023-02-06 13:52:38 +0300

    some fix and add eureka client

--- src/main/java/com/github/youtubeAPI/YoutubeApiApplication.java
+++ src/main/java/com/github/youtubeAPI/YoutubeApiApplication.java
@@ -2,10 +2,12 @@ package com.github.youtubeAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
 public class YoutubeApiApplication {
 
     public static void main(String[] args) {
